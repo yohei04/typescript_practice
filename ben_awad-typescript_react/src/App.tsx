3ca01a185c44c5e0bf2f366118a84b26bc049494
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-26 23:39:40 +0900

    子のコンポーネントにデータを渡す

--- ben_awad-typescript_react/src/App.tsx
+++ ben_awad-typescript_react/src/App.tsx
@@ -2,31 +2,20 @@ import React from 'react';
 import './App.css';
 // import { TextField } from './TextField';
 
+interface Props {
+  message: string
+}
+
+const Child: React.FC<Props> = props => {
+  return (
+    <p>{ props.message }</p>
+  )
+}
+
 const App: React.FC = () => {
-  interface Item {
-    id: number
-    title: string
-  }
-
-  const items: Item[] = [
-    {
-      id: 1,
-      title: '一番高い商品'
-    },
-    {
-      id: 2,
-      title: '一番ダサい商品'
-    }
-  ]
-
-  const message: string = 'こんにちは React!!'
   return (
     <div>
-      <ul>
-        {items.map((item: Item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <Child message="子のコンポーネントに渡す" />
     </div>
   )
 }
