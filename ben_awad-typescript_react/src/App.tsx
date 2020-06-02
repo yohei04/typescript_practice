77a20dea13cee95084f1ce8e92adcb2c34761adb
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-02 23:49:10 +0900

    in the middle of no where

--- ben_awad-typescript_react/src/App.tsx
+++ ben_awad-typescript_react/src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 // import { TextField } from './TextField';
+import Child from './components/Child'
 
-interface Props {
-  message: string
-}
 
-const Child: React.FC<Props> = props => {
-  return (
-    <p>{ props.message }</p>
-  )
-}
+
 
 const App: React.FC = () => {
   return (
     <div>
-      <Child message="子のコンポーネントに渡す" />
+      <Child />
     </div>
   )
 }
---
+++ ben_awad-typescript_react/src/components/Child.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Child: React.FC = () => {
+  return (
+    <div>子コンポーネント</div>
+  )
+}
+
+export default Child
