b4a676c6ac569398e136284ae74052a175bcf2d6
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-04 02:56:27 +0900

    finish comlete button

--- ben_awad-typescript_react/src/App.tsx
+++ ben_awad-typescript_react/src/App.tsx
@@ -19,20 +19,25 @@ const App: React.FC = () => {
   };
 
   const addTodo = (text: string): void => {
-    const newTodo: ITodo[] = [...todos, { text, complete: false }];
-    setTodos(newTodo);
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
   };
 
   const completeTodo = (index: number): void => {
-    const newTodos: ITodo[] = todos;
+    const newTodos: ITodo[] = [...todos];
     newTodos[index].complete = !newTodos[index].complete;
     setTodos(newTodos);
   };
 
+  const deleteTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    
+  }
+
   return (
     <>
       <h1>Todo List</h1>
-      <form onClick={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={value}
@@ -43,10 +48,13 @@ const App: React.FC = () => {
       <section>
         {todos.map((todo: ITodo, index: number) => (
           <div key={index}>
-            <p>{todo.text}</p>
+            <p style={{ textDecoration: todo.complete ? "line-through" : "" }}>
+              {todo.text}
+            </p>
             <button type="button" onClick={() => completeTodo(index)}>
               {todo.complete ? "Incomplete" : "complete"}
             </button>
+            <button>delete</button>
           </div>
         ))}
       </section>
