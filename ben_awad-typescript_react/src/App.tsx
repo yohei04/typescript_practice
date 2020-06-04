96f1db9d6c74622296442f47ca14e4bdb8c48ded
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-04 09:43:26 +0900

    before starting  2nd project on Udemy

--- ben_awad-typescript_react/src/App.tsx
+++ ben_awad-typescript_react/src/App.tsx
@@ -1,71 +1,97 @@
-import React, { useState } from "react";
-import "./App.css";
 
-type FormElem = React.MouseEvent<HTMLFormElement, MouseEvent>;
+import React from 'react'
 
-interface ITodo {
-  text: string;
-  complete: boolean;
+const App = () => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  )
 }
 
-const App: React.FC = () => {
-  const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<ITodo[]>([]);
+export default App
 
-  const handleSubmit = (e: FormElem): void => {
-    e.preventDefault();
-    addTodo(value);
-    setValue("");
-  };
 
-  const addTodo = (text: string): void => {
-    const newTodos: ITodo[] = [...todos, { text, complete: false }];
-    setTodos(newTodos);
-  };
 
-  const completeTodo = (index: number): void => {
-    const newTodos: ITodo[] = [...todos];
-    newTodos[index].complete = !newTodos[index].complete;
-    setTodos(newTodos);
-  };
 
-  const deleteTodo = (index: number): void => {
-    const newTodos: ITodo[] = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
 
-  return (
-    <>
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
-      <section>
-        {todos.map((todo: ITodo, index: number) => (
-          <div key={index}>
-            <p style={{ textDecoration: todo.complete ? "line-through" : "" }}>
-              {todo.text}
-            </p>
-            <button type="button" onClick={() => completeTodo(index)}>
-              {todo.complete ? "Incomplete" : "complete"}
-            </button>
-            <button type="button" onClick={() => deleteTodo(index)}>
-              delete
-            </button>
-          </div>
-        ))}
-      </section>
-    </>
-  );
-};
 
-export default App;
+
+// Udemy Todo
+
+// import React, { useState } from "react";
+// import "./App.css";
+
+// type FormElem = React.MouseEvent<HTMLFormElement, MouseEvent>;
+
+// interface ITodo {
+//   text: string;
+//   complete: boolean;
+// }
+
+// const App: React.FC = () => {
+//   const [value, setValue] = useState<string>("");
+//   const [todos, setTodos] = useState<ITodo[]>([]);
+
+//   const handleSubmit = (e: FormElem): void => {
+//     e.preventDefault();
+//     addTodo(value);
+//     setValue("");
+//   };
+
+//   const addTodo = (text: string): void => {
+//     const newTodos: ITodo[] = [...todos, { text, complete: false }];
+//     setTodos(newTodos);
+//   };
+
+//   const completeTodo = (index: number): void => {
+//     const newTodos: ITodo[] = [...todos];
+//     newTodos[index].complete = !newTodos[index].complete;
+//     setTodos(newTodos);
+//   };
+
+//   const deleteTodo = (index: number): void => {
+//     const newTodos: ITodo[] = [...todos];
+//     newTodos.splice(index, 1);
+//     setTodos(newTodos);
+//   };
+
+//   return (
+//     <>
+//       <h1>Todo List</h1>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={value}
+//           onChange={(e) => setValue(e.target.value)}
+//         />
+//         <button type="submit">Add Todo</button>
+//       </form>
+//       <section>
+//         {todos.map((todo: ITodo, index: number) => (
+//           <div key={index}>
+//             <p style={{ textDecoration: todo.complete ? "line-through" : "" }}>
+//               {todo.text}
+//             </p>
+//             <button type="button" onClick={() => completeTodo(index)}>
+//               {todo.complete ? "Incomplete" : "complete"}
+//             </button>
+//             <button type="button" onClick={() => deleteTodo(index)}>
+//               delete
+//             </button>
+//           </div>
+//         ))}
+//       </section>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+
+
+// WEBOTV
 
 // import TaskList from "./WEBOTV/components/TaskList";
 // import { Task } from "./Types";
