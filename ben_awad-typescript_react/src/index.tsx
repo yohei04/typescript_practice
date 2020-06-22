eee7dcfd38341dc2290f87cb19e6f3d82ce69a00
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-22 22:22:08 +0900

    before starting Todo DevTuts

--- ben_awad-typescript_react/src/index.tsx
+++ ben_awad-typescript_react/src/index.tsx
@@ -1,14 +1,13 @@
 import React, { JSXElementConstructor } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './Todo-second/App';
 
 
 ReactDOM.render(
-  <>
+  <React.StrictMode>
     <App />
-  </>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
@@ -35,4 +34,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
