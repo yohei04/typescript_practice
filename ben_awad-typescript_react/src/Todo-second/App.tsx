7a432ab9962b895e13ea1efd5dfa8ab9d4b6c416
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-22 23:24:24 +0900

    add TodoListItemProps compornent

--- ben_awad-typescript_react/src/Todo-second/App.tsx
+++ ben_awad-typescript_react/src/Todo-second/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { TodoListItem } from './components/TodoListItem';
+
+const todos: Array<Todo> = [
+  { text: 'Walk the dog', complete: true },
+  { text: 'Write app', complete: false },
+];
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <TodoListItem />
     </div>
   );
 };
---
+++ ben_awad-typescript_react/src/Todo-second/components/TodoListItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface TodoListItemProps {
+  todo: Todo
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+  return (
+    <li>
+      <label>
+        <input type="checkbox" checked={todo.complete} />
+        {todo.text}
+      </label>
+    </li>
+  );
+};
