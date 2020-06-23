f15a5b1c3aaa4d35be049261bf6997c424e0e78f
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-23 11:10:27 +0900

    before starting hypertextcandy-Todo

---
+++ ben_awad-typescript_react/src/hypertextcandy-Todo/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './components/Todo'
+
+const App: React.FC = ({}) => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+}
+export default App
--- ben_awad-typescript_react/src/index.tsx
+++ ben_awad-typescript_react/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Todo-second/App';
+import App from './hypertextcandy-Todo/components/Todo';
 
 
 ReactDOM.render(
