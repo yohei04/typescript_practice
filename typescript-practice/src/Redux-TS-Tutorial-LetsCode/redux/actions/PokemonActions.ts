cd164ab5aa4a221fc2992ec90cba2893268560fc
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-26 15:11:42 +0900

    set up PokemonActions

---
+++ typescript-practice/src/Redux-TS-Tutorial-LetsCode/redux/actions/PokemonActions.ts
@@ -0,0 +1,27 @@
+import { Dispatch } from 'redux';
+import {
+  PokemonDispatchTypes,
+  POKEMON_FAIL,
+  POKEMON_SUCCESS,
+  POKEMON_LOADING,
+} from './PokemonActionTypes';
+import axios from 'axios';
+
+export const GetPokemon = (pokemon: string) => async (
+  dispatch: Dispatch<PokemonDispatchTypes>
+) => {
+  try {
+    dispatch({
+      type: POKEMON_LOADING,
+    });
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    dispatch({
+      type: POKEMON_SUCCESS,
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: POKEMON_FAIL,
+    });
+  }
+};
