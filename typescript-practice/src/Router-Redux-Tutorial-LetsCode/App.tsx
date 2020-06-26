ea9beff6425cad824f668e28b7ee0ba3e7bb40b8
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-26 20:55:13 +0900

    add Router and nav

--- typescript-practice/src/Router-Redux-Tutorial-LetsCode/App.tsx
+++ typescript-practice/src/Router-Redux-Tutorial-LetsCode/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import Store from './Store';
-import { BrowserRouter } from 'react-router-dom';
+import './App.css'
+import { Switch, Route, NavLink, Redirect } from 'react-router-dom';
+import PokemonList from './containers/PokemonList';
+import Pokemon from './containers/Pokemon';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Provider store={Store}>test</Provider>
-    </BrowserRouter>
+    <div className="App">
+      <nav>
+        <NavLink to={'/'}>Search</NavLink>
+      </nav>
+      <Switch>
+        <Route path={'/'} exact component={PokemonList} />
+        <Route path={'/pokemon/:pokemon'} exact component={Pokemon} />
+        <Redirect to={'/'} />
+      </Switch>
+    </div>
   );
 };
 
