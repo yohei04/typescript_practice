25a6b4c71d270188b0af980c8865f108557db11e
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-26 16:11:10 +0900

    set up PokemonReducer

--- typescript-practice/src/Redux-TS-Tutorial-LetsCode/redux/actions/PokemonActionTypes.ts
+++ typescript-practice/src/Redux-TS-Tutorial-LetsCode/redux/actions/PokemonActionTypes.ts
@@ -2,6 +2,12 @@ export const POKEMON_LOADING = 'POKEMON_LOADING';
 export const POKEMON_FAIL = 'POKEMON_FAIL';
 export const POKEMON_SUCCESS = 'POKEMON_SUCCESS';
 
+export type PokemonType = {
+  abilities: PokemonAbility[];
+  sprites: PokemonSprites;
+  stats: PokemonStat[];
+};
+
 export type PokemonAbility = {
   ability: {
     name: string;
@@ -30,11 +36,7 @@ export interface PokemonFail {
 
 export interface PokemonSuccess {
   type: typeof POKEMON_SUCCESS;
-  payload: {
-    abilities: PokemonAbility[];
-    sprites: PokemonSprites;
-    stats: PokemonStat[];
-  };
+  payload: PokemonType;
 }
 
 export type PokemonDispatchTypes =
--- typescript-practice/src/Redux-TS-Tutorial-LetsCode/redux/reducers/PokemonReducer.ts
+++ typescript-practice/src/Redux-TS-Tutorial-LetsCode/redux/reducers/PokemonReducer.ts
@@ -1,12 +1,41 @@
-interface DefaultStateI {}
+import { PokemonType } from '../actions/PokemonActionTypes';
+import {
+  PokemonDispatchTypes,
+  POKEMON_FAIL,
+  POKEMON_SUCCESS,
+  POKEMON_LOADING,
+} from '../actions/PokemonActionTypes';
 
-const defaultState: DefaultStateI = {};
+interface DefaultStateI {
+  loading: boolean;
+  pokemon?: PokemonType;
+}
+
+const defaultState: DefaultStateI = {
+  loading: false,
+};
 
 const pokemonReducer = (
   state: DefaultStateI = defaultState,
-  action: any
+  action: PokemonDispatchTypes
 ): DefaultStateI => {
-  return state;
+  switch (action.type) {
+    case POKEMON_LOADING:
+      return {
+        loading: true,
+      };
+    case POKEMON_SUCCESS:
+      return {
+        loading: false,
+        pokemon: action.payload,
+      };
+    case POKEMON_FAIL:
+      return {
+        loading: false,
+      };
+    default:
+      return state;
+  }
 };
 
 export default pokemonReducer;
