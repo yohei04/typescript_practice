a89c3c314ddd45ceb85e4195cff7c48ad5bf6a71
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-28 17:07:05 +0900

    add pokemonListReducer and type

---
+++ typescript-practice/src/Router-Redux-Tutorial-LetsCode/redux/reducers/pokemonListReducers.ts
@@ -0,0 +1,46 @@
+import {
+  POKEMON_LIST_LOADING,
+  POKEMON_LIST_SUCCESS,
+  POKEMON_LIST_FAILURE,
+} from '../actions/pokemonActionTypes';
+
+interface DefaultState {
+  loading: boolean;
+  data: any;
+  errorMsg: string;
+}
+
+const defaultState: DefaultState = {
+  loading: false,
+  data: [],
+  errorMsg: '',
+};
+
+const pokemonListReducer = (state = defaultState, action: any) => {
+  switch (action.type) {
+    case POKEMON_LIST_LOADING:
+      return {
+        ...state,
+        loading: true,
+        errorMsg: '',
+      };
+    case POKEMON_LIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        errorMsg: '',
+      };
+    case POKEMON_LIST_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        errorMsg: 'unable to get pokemon',
+      };
+
+    default:
+      break;
+  }
+};
+
+export default pokemonListReducer;
