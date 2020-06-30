a518a23f7ef103696310ce80ca1f6db98c483a4c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-30 10:00:01 +0900

    add interfaces

--- typescript-practice/src/Router-Redux-Tutorial-LetsCode/redux/actions/pokemonActionTypes.ts
+++ typescript-practice/src/Router-Redux-Tutorial-LetsCode/redux/actions/pokemonActionTypes.ts
@@ -2,3 +2,20 @@ export const POKEMON_LIST_LOADING = 'POKEMON_LIST_LOADING';
 export const POKEMON_LIST_SUCCESS = 'POKEMON_LIST_SUCCESS';
 export const POKEMON_LIST_FAILURE = 'POKEMON_LIST_FAILURE';
 
+interface PokemonLoading {
+  type: typeof POKEMON_LIST_LOADING;
+}
+
+interface PokemonSuccess {
+  type: typeof POKEMON_LIST_SUCCESS;
+  payload: any;
+}
+
+interface PokemonFailure {
+  type: typeof POKEMON_LIST_FAILURE;
+}
+
+export type PokemonDispatchTypes =
+  | PokemonLoading
+  | PokemonSuccess
+  | PokemonFailure;
