3586c24fcf16c93fd1a881cd4d1a81e7fe014618
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-01 17:10:10 +0900

    add Pokemon component

--- typescript-practice/src/Router-Redux-Tutorial-LetsCode/containers/Pokemon.tsx
+++ typescript-practice/src/Router-Redux-Tutorial-LetsCode/containers/Pokemon.tsx
@@ -1,8 +1,62 @@
-import React from 'react';
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {GetPokemon} from "../redux/actions/pokemonActions";
+import _ from "lodash";
+import { RootState } from "../Store";
 
 const Pokemon = (props: any) => {
-  console.log(props)
-  return <div>Pokemon</div>;
+  const pokemonName = props.match.params.pokemon;
+  const dispatch = useDispatch();
+  const pokemonState = useSelector((state: RootState) => state.pokemon);
+  React.useEffect(() => {
+    dispatch(GetPokemon(pokemonName))
+  }, []);
+
+  const ShowData = () => {
+    if (!_.isEmpty(pokemonState.data[pokemonName])) {
+      const pokeData = pokemonState.data[pokemonName];
+      return(
+        <div className={"pokemon-wrapper"}>
+          <div className={"item"}>
+            <h1>Sprites</h1>
+            <img src={pokeData.sprites.front_default} alt=""/>
+            <img src={pokeData.sprites.back_default} alt=""/>
+            <img src={pokeData.sprites.front_shiny} alt=""/>
+            <img src={pokeData.sprites.back_shiny} alt=""/>
+          </div>
+          <div className="item">
+            <h1>Stats</h1>
+            {pokeData.stats.map((el: any) => {
+              return <p>{el.stat.name} {el.base_stat}</p>
+            })}
+          </div>
+          <div className="item">
+            <h1>Abilities</h1>
+            {pokeData.abilities.map((el: any) => {
+              return <p>{el.ability.name}</p>
+            })}
+          </div>
+        </div>
+      )
+    }
+
+    if (pokemonState.loading) {
+      return <p>Loading...</p>
+    }
+
+    if (pokemonState.errMsg !== "") {
+      return <p>{pokemonState.errMsg}</p>
+    }
+
+    return <p>error getting pokemon</p>
+  }
+
+  return(
+    <div className={"poke"}>
+      <h1>{pokemonName}</h1>
+      {ShowData()}
+    </div>
+  )
 };
 
-export default Pokemon;
+export default Pokemon
