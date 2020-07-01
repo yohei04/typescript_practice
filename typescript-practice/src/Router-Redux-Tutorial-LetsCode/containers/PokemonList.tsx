53474248375afbe57f61e6550deb4a8671954f28
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-01 10:56:34 +0900

    add FetchData

--- typescript-practice/src/Router-Redux-Tutorial-LetsCode/containers/PokemonList.tsx
+++ typescript-practice/src/Router-Redux-Tutorial-LetsCode/containers/PokemonList.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../Store';
+import _ from 'lodash';
+import { GetPokemonList } from '../redux/actions/pokemonActions';
+import { Link } from 'react-router-dom';
 
 const PokemonList: React.FC = () => {
   const dispatch = useDispatch();
   const pokemonList = useSelector((state: RootState) => state.pokemonList);
-  return <div>Pokemon List</div>;
+
+  useEffect(() => {
+    FetchData(1);
+  }, []);
+
+  const FetchData = (page = 1) => {
+    dispatch(GetPokemonList(page));
+  };
+
+  const ShowData: any = () => {
+    if (!_.isEmpty(pokemonList.data)) {
+      return (
+        <div className="list-wrapper">
+          {pokemonList.data.map((el: any) => {
+            return (
+              <div className='pokemon.item'>
+                <p>{el.name}</p>
+                <Link to={`/pokemon/${el.name}`}>View</Link>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
+    if (pokemonList.loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (pokemonList.errorMsg !== '') {
+      return <p>{pokemonList.errorMsg}</p>;
+    }
+
+    return <p>unable to get data</p>;
+  };
+
+  return <div>{ShowData()}</div>;
 };
 
 export default PokemonList;
--- typescript-practice/src/Router-Redux-Tutorial-LetsCode/redux/reducers/pokemonListReducers.ts
+++ typescript-practice/src/Router-Redux-Tutorial-LetsCode/redux/reducers/pokemonListReducers.ts
@@ -2,23 +2,26 @@ import {
   POKEMON_LIST_LOADING,
   POKEMON_LIST_SUCCESS,
   POKEMON_LIST_FAILURE,
+  PokemonDispatchTypes,
 } from '../actions/pokemonActionTypes';
 
 interface DefaultState {
   loading: boolean;
   data: any;
   errorMsg: string;
+  count: number;
 }
 
 const defaultState: DefaultState = {
   loading: false,
   data: [],
   errorMsg: '',
+  count: 0,
 };
 
 const pokemonListReducer = (
   state: DefaultState = defaultState,
-  action: any
+  action: PokemonDispatchTypes
 ) => {
   switch (action.type) {
     case POKEMON_LIST_LOADING:
@@ -31,8 +34,9 @@ const pokemonListReducer = (
       return {
         ...state,
         loading: false,
-        data: action.payload,
+        data: action.payload.results,
         errorMsg: '',
+        count: action.payload.count,
       };
     case POKEMON_LIST_FAILURE:
       return {
