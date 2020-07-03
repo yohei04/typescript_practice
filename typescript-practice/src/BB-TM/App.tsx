553ec5fc34d0af1f527c7c23b5c9c90b315b3ae7
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-03 13:16:10 +0900

    add setItems

--- typescript-practice/src/BB-TM/App.tsx
+++ typescript-practice/src/BB-TM/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import Header from './components/ui/Header'
+import { result } from 'lodash'
+import axios from 'axios'
 
 const App = () => {
+  const [items, setItems] = useState<any>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const result = await axios.get(`https://www.breakingbadapi.com/api/characters`);
+
+      console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItem()
+  }, [])
+
   return (
-    <div>
-      test
+    <div className='container'>
+      <Header />
     </div>
   )
 }
