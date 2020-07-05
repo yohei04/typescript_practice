58c9c76fb7a613c4d5322ef405bfcbb66a42985c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-05 15:26:12 +0900

    add BookList and Navbar components

---
+++ typescript-practice/src/NN-Context&Hooks/components/BookList.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>the way of kings</li>
+        <li>the name of the wind</li>
+        <li>the final empire</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList
+
--- typescript-practice/src/NN-Context&Hooks/components/Navbar.tsx
+++ typescript-practice/src/NN-Context&Hooks/components/Navbar.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
+import { Themes } from '../types'
 
 const Navbar = () => {
+  const themes: Themes = useContext(ThemeContext)
+  const { isLightTheme, light, dark } = themes
+  const theme = isLightTheme ? light : dark
   return (
-    <nav>
+    <nav style={{background: theme.ui, color: theme.syntax}} >
       <h1>Context App</h1>
       <ul>
         <li>Home</li>
