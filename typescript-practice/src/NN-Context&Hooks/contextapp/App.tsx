5bbf85b8655426d5b2b42d491eb2ed0e5e7b4de5
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-05 21:34:32 +0900

    move all files to contexapp folder

--- typescript-practice/src/NN-Context&Hooks/contextapp/App.tsx
+++ typescript-practice/src/NN-Context&Hooks/contextapp/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import './App.css';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
--- typescript-practice/src/NN-Context&Hooks/contextapp/contexts/ThemeContext.tsx
+++ typescript-practice/src/NN-Context&Hooks/contextapp/contexts/ThemeContext.tsx
@@ -1,15 +1,22 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import { Themes } from '../types';
 
-const themes: Themes = {
+const initialThemes: Themes = {
   isLightTheme: true,
   light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
   dark: { syntax: '#ddd', ui: '#333', bg: '#555' },
 };
 
-export const ThemeContext = createContext(themes);
+export const ThemeContext = createContext(initialThemes);
+
 
 const ThemeContextProvider: React.FC = (props) => {
+  const [themes, setThemes] = useState(initialThemes);
+  
+  const toggleThemes = () => {
+    setThemes({ ...themes, isLightTheme: !themes.isLightTheme });
+  }
+
   return (
     <>
       <ThemeContext.Provider value={themes}>
