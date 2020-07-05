97d2780515d1b521146c30ea00a9cd7a1e4fea40
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-05 15:26:35 +0900

    add ContextProvider

--- typescript-practice/src/NN-Context&Hooks/App.tsx
+++ typescript-practice/src/NN-Context&Hooks/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import './App.css';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
