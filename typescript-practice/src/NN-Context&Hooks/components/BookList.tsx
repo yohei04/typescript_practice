4be5ca4cec18d6e87d133c73f1bed7a0de0f3fe2
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-05 15:41:52 +0900

    add thems in BookList component

--- typescript-practice/src/NN-Context&Hooks/components/BookList.tsx
+++ typescript-practice/src/NN-Context&Hooks/components/BookList.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 const BookList = () => {
+  const themes = useContext(ThemeContext)
+  const { isLightTheme, light, dark } = themes
+  const theme = isLightTheme ? light : dark
+
   return (
-    <div className="book-list">
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
       <ul>
-        <li>the way of kings</li>
-        <li>the name of the wind</li>
-        <li>the final empire</li>
+        <li style={{background: theme.ui}}>the way of kings</li>
+        <li style={{background: theme.ui}}>the name of the wind</li>
+        <li style={{background: theme.ui}}>the final empire</li>
       </ul>
     </div>
-  )
+  );
 }
 
 export default BookList
